8bfda1ac6cd4507acffa6826a69a97e6ef55581e
José Silva <jose.silva@modernasistemas.com.br>
2021-11-20 11:45:45 -0300

    Corrigido com novo projeto em MVC para versão correta do framework

---
+++ DataPlusMVCApp/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DataPlusMVCApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "";
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "";
+
+            return RedirectToAction("Index");
+        }
+    }
+}
